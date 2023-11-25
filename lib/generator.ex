73a88ddf5f579d0031439781bca74c034a26bffa
Tom Monks <monksy@gmail.com>
2023-11-25 12:26:33 -0500

    Use openai library directly

--- lib/generator.ex
+++ lib/generator.ex
@@ -2,11 +2,15 @@ defmodule KindQuiz.Generator do
   @moduledoc """
   Generatez quizzes and questions using OpenAI API
   """
-  import AI
 
   alias KindQuiz.Quizzes.Quiz
   alias KindQuiz.Quizzes.Question
   alias KindQuiz.Repo
+
+  @models %{
+    gpt3: "gpt-3.5-turbo-1106",
+    gtp4: "gpt-4-1106-preview"
+  }
 
   @doc """
   Generates a category quiz
@@ -19,9 +23,8 @@ defmodule KindQuiz.Generator do
         outcomes
       end
 
-    ~l"""
-    model: gpt-3.5-turbo
-    system: You are a quiz generator that generates fun quizzes for 10-14 year old children.
+    system_prompt = """
+    You are a quiz generator that generates fun quizzes for 10-14 year old children.
     The quiz will attempt to categorize the taker in some way based on their responses.
     Each quiz should have 5 questions.
     Each quiz will have a certain number of possible outcomes (i.e. categories that the taker can be placed in).
@@ -37,7 +40,7 @@ defmodule KindQuiz.Generator do
 
     You:
 
-    ```
+    ``
     {
       "title": "What kind of superhero are you?",
       "type": "category",
@@ -63,12 +66,17 @@ defmodule KindQuiz.Generator do
       // 4 more questions...
     }
     ```
-    user: Title: #{quiz_title}, Outcomes: #{outcome_text}
     """
-    |> chat()
-    |> decode_response()
-    # |> IO.inspect()
-    |> create_changeset()
+
+    user_prompt = "Title: #{quiz_title}, Outcomes: #{outcome_text}"
+
+    get_completion(@models[:gpt3], system_prompt, user_prompt,
+      temperature: 0.8,
+      response_format: %{type: "json_object"}
+    )
+    |> parse_chat()
+    |> decode_json()
+    |> create_quiz_changeset()
     |> Repo.insert()
   end
 
@@ -79,47 +87,67 @@ defmodule KindQuiz.Generator do
     quiz = Repo.reload(quiz) |> Repo.preload(:questions)
     previous_questions = quiz.questions |> Enum.map(& &1.text) |> Enum.join(", ")
 
-    ~l"""
-    model: gpt-4-1106-preview
-    system: You are a quiz question generator that generates fun and interesting trivia questions"
-    Each question on should have 4 possible answers, numbered 1-4.
-    The questions should be of a difficulty level appropriate for an adult.
-    Each question should have a brief explanation about the correct answer.
-    I will give you the subject of the trivia and you will generate the data for a trivia quiz question on that topic.
-    I will provide you with a list of previous questions so that you can avoid repeating them.
-    Please generate the question in JSON format like the example below.
-    Respond ONLY with the JSON with no additional text.
+    system_prompt =
+      """
+        You are a quiz question generator that generates fun and interesting trivia questions"
+        Each question on should have 4 possible answers, numbered 1-4.
+        The questions should be of a difficulty level appropriate for an adult.
+        Each question should have a brief explanation about the correct answer.
+        I will give you the subject of the trivia and you will generate the data for a trivia quiz question on that topic.
+        I will provide you with a list of previous questions so that you can avoid repeating them.
+        Please generate the question in JSON format like the example below.
+        Respond ONLY with the JSON with no additional text.
 
-    User: "Subject: Interesting insects. Previous questions: What is the fastest insect?"
+        User: "Subject: Interesting insects. Previous questions: What is the fastest insect?"
 
-    You:
+        You:
 
-    {
-      "text": "The praying mantid can move only the top part of this",
-      "correct": 4,
-      "explanation": "This ability allows them to sneak up on prey without startling it",
-      "answers": [
-        { "text": "arms", "number": 1 },
-        { "text": "legs", "number": 2 },
-        { "text": "eyes", "number": 3 },
-        { "text": "body", "number": 4 }
-      ]
-    }
+        {
+          "text": "The praying mantid can move only the top part of this",
+          "correct": 4,
+          "explanation": "This ability allows them to sneak up on prey without startling it",
+          "answers": [
+            { "text": "arms", "number": 1 },
+            { "text": "legs", "number": 2 },
+            { "text": "eyes", "number": 3 },
+            { "text": "body", "number": 4 }
+          ]
+        }
+      """
 
-    User: Subject: #{title}. Previous questions: #{previous_questions}
-    """
-    |> chat()
-    |> IO.inspect()
-    |> decode_response()
+    user_prompt = "Subject: #{title}. Previous questions: #{previous_questions}"
+
+    get_completion(@models[:gpt3], system_prompt, user_prompt,
+      temperature: 0.8,
+      response_format: %{type: "json_object"}
+    )
+    |> parse_chat()
+    |> decode_json()
     |> create_question_changeset(quiz)
     |> Repo.insert()
   end
 
-  defp decode_response({:ok, json}) do
+  def get_completion(model, system_prompt, user_prompt, options \\ []) do
+    messages = [
+      %{role: "system", content: system_prompt},
+      %{role: "user", content: user_prompt}
+    ]
+
+    args = Keyword.merge([model: model, messages: messages], options)
+
+    OpenAI.chat_completion(args)
+  end
+
+  defp parse_chat({:ok, %{choices: [%{"message" => %{"content" => content}} | _]}}),
+    do: {:ok, content}
+
+  defp parse_chat({:error, %{"error" => %{"message" => message}}}), do: {:error, message}
+
+  defp decode_json({:ok, json}) do
     Jason.decode!(json)
   end
 
-  defp create_changeset(attrs) do
+  defp create_quiz_changeset(attrs) do
     Quiz.changeset(%Quiz{}, attrs)
   end
 
