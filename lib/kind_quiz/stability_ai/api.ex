eed863a8f84d5c8fa481fbddf8a7ccd498fc0503
Tom Monks <monksy@gmail.com>
2023-12-16 13:19:29 -0500

    Api.generate_image filename and file writing

--- lib/kind_quiz/stability_ai/api.ex
+++ lib/kind_quiz/stability_ai/api.ex
@@ -1,8 +1,18 @@
 defmodule KindQuiz.StabilityAi.Api do
   alias KindQuiz.StabilityAi.Client
 
-  def generate_image(prompt) do
-    url = "https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image"
+  @doc """
+  Generates and saves an image for the given prompt.
+  Returns the filename.
+
+  Possible params:
+
+  - style_preset: 3d-model, analog-film, anime, cinematic, comic-book, digital-art, enhance, fantasy-art, isometric, line-art, low-poly, modeling-compound, neon-punk, origami, photographic, pixel-art, tile-texture
+  """
+  def generate_image(prompt, params \\ %{}) do
+    model = "stable-diffusion-xl-1024-v1-0"
+    url = "https://api.stability.ai/v1/generation/#{model}/text-to-image"
+    download_path = Application.get_env(:quiz, :download_path)
 
     headers =
       [
@@ -11,23 +21,37 @@ defmodule KindQuiz.StabilityAi.Api do
         {"Accept", "application/json"}
       ]
 
-    timeout = 120_000
+    body =
+      %{
+        steps: 40,
+        width: 1024,
+        height: 1024,
+        seed: 0,
+        cfg_scale: 6,
+        samples: 1,
+        style_preset: "enhance",
+        text_prompts: [
+          %{text: prompt, weight: 1},
+          %{text: "blurry, bad", weight: -1}
+        ]
+      }
+      |> Map.merge(params)
 
-    body = %{
-      steps: 40,
-      width: 1024,
-      height: 1024,
-      seed: 0,
-      cfg_scale: 5,
-      samples: 1,
-      style_preset: "enhance",
-      text_prompts: [
-        %{text: prompt, weight: 1},
-        %{text: "blurry, bad", weight: -1}
-      ]
-    }
+    {:ok, %Req.Response{body: resp_body}} =
+      Client.post(url, json: body, headers: headers, receive_timeout: 120_000)
 
-    Client.post(url, json: body, headers: headers, receive_timeout: timeout)
+    %{"artifacts" => [%{"base64" => base64, "seed" => seed}]} = resp_body
+    image_data = Base.decode64!(base64)
+    filename = make_filename(body.style_preset, seed, prompt)
+    File.write!("#{download_path}/#{filename}", image_data)
+    {:ok, filename}
+  end
+
+  defp make_filename(style_preset, seed, prompt) do
+    prompt = prompt |> String.replace(" ", "_")
+    base_name = "SD-#{style_preset}-#{seed}-#{prompt}"
+    # truncate to 255 characters total
+    (base_name |> String.slice(0..250)) <> ".png"
   end
 
   defp get_api_key() do
--- test/quiz/stability_ai/api_test.exs
+++ test/quiz/stability_ai/api_test.exs
@@ -5,7 +5,7 @@ defmodule KindQuiz.StabilityAi.ApiTest do
   alias KindQuiz.StabilityAi.Api
   alias KindQuiz.StabilityAi.MockClient
 
-  setup [:verify_on_exit!, :set_api_key]
+  setup [:verify_on_exit!, :setup_environment]
 
   describe "generate_image/1" do
     test "sends a post request with the expected params to the API" do
@@ -17,13 +17,27 @@ defmodule KindQuiz.StabilityAi.ApiTest do
         assert {"Authorization", "Bearer StaB1l1tyA1"} in options[:headers]
         assert {"Content-Type", "application/json"} in options[:headers]
         assert {"Accept", "application/json"} in options[:headers]
+
+        {:ok,
+         %Req.Response{
+           body: %{
+             "artifacts" => [
+               %{
+                 "base64" => "Zm9vYmFy",
+                 "seed" => 12345
+               }
+             ]
+           }
+         }}
       end)
 
-      assert {:ok, "something"} = Api.generate_image("a cute fluffy cat")
+      assert {:ok, "SD-awesome-12345-a_cute_fluffy_cat.png"} =
+               Api.generate_image("a cute fluffy cat", %{style_preset: "awesome"})
     end
   end
 
-  defp set_api_key(_) do
-    Application.put_env(:kind_quiz, :stability_ai_api_key, "StaB1l1tyA1")
+  defp setup_environment(_) do
+    Application.put_env(:quiz, :stability_ai_api_key, "StaB1l1tyA1")
+    Application.put_env(:quiz, :download_path, "/tmp")
   end
 end
