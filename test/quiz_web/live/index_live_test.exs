247cbd1bd5a96249598a4814ab2fbc7a60e89644
Tom Monks <monksy@gmail.com>
2024-02-07 09:48:19 -0500

    IndexLive admin mode

--- test/quiz_web/live/index_live_test.exs
+++ test/quiz_web/live/index_live_test.exs
@@ -3,9 +3,10 @@ defmodule KQWeb.IndexLiveTest do
   import Phoenix.LiveViewTest
   import KQ.Factory
 
+  alias KQ.Quizzes
+
   describe "as normal user" do
     test "renders a card for each quiz", %{conn: conn} do
-      # conn = put_session(conn, "authenticated", "banana")
       quiz1 = insert(:quiz, title: "What kind of pizza are you?")
       quiz2 = insert(:quiz, title: "What kind of book are you?")
       {:ok, view, _html} = live(conn, "/")
@@ -59,18 +60,56 @@ defmodule KQWeb.IndexLiveTest do
       assert has_element?(view, "#quiz-#{quiz.id} img[src='images/quiz/pizza.png']")
     end
 
-    test "does not show 'Hi admin' message when not authenticated", %{conn: conn} do
+    test "does not show inactive quizzes", %{conn: conn} do
+      inactive_quiz = insert(:quiz, is_active: false)
       {:ok, view, _html} = live(conn, "/")
-      refute has_element?(view, "#admin-message", "Hi admin")
+
+      refute has_element?(view, "#quiz-#{inactive_quiz.id}")
     end
   end
 
   describe "as admin" do
     setup :login
 
-    test "shows 'Hi admin' message when authenticated", %{conn: conn} do
+    test "shows inactive quizzes", %{conn: conn} do
+      inactive_quiz = insert(:quiz, is_active: false)
       {:ok, view, _html} = live(conn, "/")
-      assert has_element?(view, "#admin-message", "Hi admin")
+
+      assert has_element?(view, "#quiz-#{inactive_quiz.id}")
+    end
+
+    test "has a checkbox on each card to toggle the quiz's active status", %{conn: conn} do
+      quiz = insert(:quiz, is_active: true)
+      {:ok, view, _html} = live(conn, "/")
+
+      assert has_element?(view, "#quiz-#{quiz.id} input[type='checkbox']")
+    end
+
+    test "the checkbox is checked if the quiz is active", %{conn: conn} do
+      active_quiz = insert(:quiz, is_active: true)
+      inactive_quiz = insert(:quiz, is_active: false)
+      {:ok, view, _html} = live(conn, "/")
+
+      assert has_element?(view, "#quiz-#{active_quiz.id} input[type='checkbox'][checked]")
+      refute has_element?(view, "#quiz-#{inactive_quiz.id} input[type='checkbox'][checked]")
+    end
+
+    test "toggling the checkbox updates the quiz's active status", %{conn: conn} do
+      quiz = insert(:quiz, is_active: false)
+      {:ok, view, _html} = live(conn, "/")
+
+      # checkbox is unchecked
+      assert has_element?(view, "#quiz-#{quiz.id} input[type='checkbox']")
+      refute has_element?(view, "#quiz-#{quiz.id} input[type='checkbox'][checked]")
+
+      view
+      |> element("#quiz-#{quiz.id} input[type='checkbox']")
+      |> render_click()
+
+      # checkbox is checked
+      assert has_element?(view, "#quiz-#{quiz.id} input[type='checkbox'][checked]")
+      # quiz is updated
+      assert Quizzes.get_quiz!(quiz.id).is_active == true
     end
 
     defp login(%{conn: conn} = context) do
